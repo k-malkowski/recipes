b82c2288dccb4477e903c99adef7a009787c3a33
Kacper Malkowski <malkowski.kacperum@gmail.com>
2021-08-08 13:16:41 +0200

    move hash password to authservice

--- src/modules/users/usersService.ts
+++ src/modules/users/usersService.ts
@@ -1,18 +1,9 @@
-import * as bcrypt from 'bcrypt';
 import { Prisma, User } from '@prisma/client';
 import usersRepository from './usersRepository';
 
 export const addUser = async (
   userData: Prisma.UserCreateInput
-): Promise<User> => {
-  const { username, password, email } = userData;
-
-  return usersRepository.add({
-    username,
-    email,
-    password: await bcrypt.hash(password, 10),
-  });
-};
+): Promise<User> => usersRepository.add(userData);
 
 export const usernameExists = async (username: string) =>
   !!(await usersRepository.findByUsername(username));
